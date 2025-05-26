072fc98169acc17d541737da2e7fb3a1c06c1cb4
scotdotwtf <77427029+scotdotwtf@users.noreply.github.com>
2025-05-25 22:24:42 -0500

    cool mustache.js menu loading
    i <3 static websites

--- interface.js
+++ interface.js
@@ -2,7 +2,7 @@
 function truncate() {
     if (window.innerWidth <= 775) {
         document.getElementById('photography').textContent = '/photo';
-    hasDvd = true;
+        hasDvd = true;
     } else if (window.innerWidth >= 775) {
         document.getElementById('photography').textContent = '/photography';
     }
@@ -12,34 +12,58 @@ function truncate() {
 addEventListener("resize", truncate);
 addEventListener("load", truncate);
 
-/* menu code 
+// automated menus
+function bindMenu(path, domain) {
+    const trigger = document.getElementById(path);
+    const bgcolor = window.getComputedStyle(trigger).getPropertyValue('background-color');
+    const acolor = window.getComputedStyle(trigger).getPropertyValue('color');
 
-    CONCEPT: CMS THIS OR SOMETHING SO THAT COLORS FOLLOW BUTTON CLICKED COLORS AND AUTOMATE
-*/
-const downloads_menu = document.getElementById('download-menu');
+    fetch(`https://${domain}/data.json`)
+        .then(res => res.json())
+        .then(data => {
+            const rendered = Mustache.render(document.getElementById('menu-template').innerHTML, {
+                id: `${path}-menu`,
+                title: path,
+                bgcolor: bgcolor,
+                items: data.map(item => ({ ...item, color: acolor }))
+            });
 
-document.getElementById('downloadables').onclick = (() => {
-    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#dee9fc');
-    downloads_menu.classList.remove('-translate-y-full');
-});
+            // string to dom
+            const tempContainer = document.createElement('div');
+            tempContainer.innerHTML = rendered;
+            const menu = tempContainer.firstElementChild;
 
-document.getElementById('d-close').onclick = (() => {
-    downloads_menu.classList.add('-translate-y-full');
-    setTimeout(() => {
-        document.querySelector('meta[name="theme-color"]').setAttribute('content', '#ffffff');
-    }, 250);
-});
+            // show/hide
+            function open() {
+                document.querySelector('meta[name="theme-color"]').setAttribute('content', bgcolor);
+                menu.classList.remove('-translate-y-full');
+            }
 
-const socials_menu = document.getElementById('social-menu');
+            if (window.location.hash == `#${path}`) {
+                open()
+            }
 
-document.getElementById('socials').onclick = (() => {
-    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#effccf');
-    socials_menu.classList.remove('-translate-y-full');
-});
+            trigger.onclick = () => {
+                if (window.location.hash != `#${path}`) {
+                    window.location.hash = path;
+                }
+                open()
+            };
 
-document.getElementById('s-close').onclick = (() => {
-    socials_menu.classList.add('-translate-y-full');
-    setTimeout(() => {
-        document.querySelector('meta[name="theme-color"]').setAttribute('content', '#ffffff');
-    }, 250);
-});
+            menu.querySelector('.close').onclick = () => {
+                if (window.location.hash === `#${path}`) {
+                    history.replaceState(null, '', window.location.pathname);
+                }
+                menu.classList.add('-translate-y-full');
+                setTimeout(() => {
+                    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#ffffff');
+                }, 265);
+            };
+
+            document.body.appendChild(menu);
+        })
+        .catch(err => console.error('error when loading menu:', err));
+}
+
+bindMenu('downloadables', 'share.scot.wtf')
+bindMenu('socials', 'socials.scot.wtf')
